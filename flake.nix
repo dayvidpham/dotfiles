41c13eed41de38304cd0b86e9d330bd8646636b4
dayvidpham <dayvidpham@gmail.com>
2023-11-20 10:07:22 +0000

    Removes unnecessary comments, lines

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A template that shows all standard flake outputs";
+  description = "Base configuration using flake to manage NixOS";
 
   # Inputs
   # https://nixos.org/manual/nix/unstable/command-ref/new-cli/nix3-flake.html#flake-inputs
@@ -7,9 +7,6 @@
   # The master branch of the NixOS/nixpkgs repository on GitHub.
   # inputs.unstable.url = "github:NixOS/nixpkgs/master";
 
-  # It is also possible to "inherit" an input from another input. This is useful to minimize
-  # flake dependencies. For example, the following sets the nixpkgs input of the top-level flake
-  # to be equal to the nixpkgs input of the nixops input of the top-level flake:
   inputs.nixpkgs.url = "nixpkgs";
   # inputs.nixpkgs.follows = "unstable";
 
