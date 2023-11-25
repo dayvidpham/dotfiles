75566092af2761d02f44b0fbbf4b1b682b1f7507
dayvidpham <dayvidpham@gmail.com>
2023-11-25 04:40:08 -0800

    Fix bungled live cd flake lock

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, hyprland, ... }: 
