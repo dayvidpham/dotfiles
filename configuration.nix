3eee81d715b5afe955eea047c414762df1fcb567
dayvidpham <dayvidpham@gmail.com>
2023-11-25 19:33:38 -0800

    Enable XServer and GDM

--- configuration.nix
+++ configuration.nix
@@ -110,21 +110,18 @@
 
   # DEPRECATED
   # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
 
   # Configure keymap in X11
-  # services.xserver = {
-  #   layout = "us";
-  #   xkbVariant = "";
-  # };
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
 
   # Enable automatic login for the user.
-  # services.xserver.displayManager.autoLogin.enable = true;
-  # services.xserver.displayManager.autoLogin.user = "dhpham";
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "dhpham";
 
 
   # Some programs need SUID wrappers, can be configured further or are
