b5501443b09fe735a705c59ef825b6c4034fa409
dayvidpham <dayvidpham@gmail.com>
2023-11-26 04:20:30 -0800

    Replace kitty with alacritty

--- virtualbox/configuration.nix
+++ virtualbox/configuration.nix
@@ -96,7 +96,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
-    kitty # For hyprland
+    alacritty
   ];
   programs.vim.defaultEditor = true;
   programs.git = {
