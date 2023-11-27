225b00f1ff45c83031b231ccb35626e0ff6d99af
dayvidpham <dayvidpham@gmail.com>
2023-11-27 00:20:45 -0800

    Adds a font, enables mtr and gnupg

--- virtualbox/configuration.nix
+++ virtualbox/configuration.nix
@@ -1,6 +1,6 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# and in the NixOS manual (accessible by running `nixos-help`).
 
 { config, pkgs, ... }:
 
@@ -10,54 +10,39 @@
       ./hardware-configuration.nix
     ];
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  system.stateVersion = "23.05"; # Did you read the comment?
-  documentation = {
-    enable = true;
-    man.enable = true;
-    man.generateCaches = true;
-    dev.enable = true;
-    info.enable = true;
-    doc.enable = true;
-    nixos.enable = true;
+  # Use the systemd-boot EFI boot loader.
+  nix = {
+    settings.experimental-features = [ "nix-command" "flakes" ];
   };
-
-
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  nixpkgs.config.allowUnfree = true;
+  system.copySystemConfiguration = true;
+  system.stateVersion = "23.05";
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "America/Vancouver";
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_CA.UTF-8";
 
-  # Enable and configure hyprland
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
+  fonts = {
+    enableDefaultFonts = true;
+    fonts = with pkgs; [
+      (nerdfonts.override { fonts = [ "iA-Writer" ]; })
+    ];
   };
-  environment.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "1";    # If you can't see mouse
-    NIXOS_OZONE_WL = "1";             # ??? from Wayland + NixOS vid
-    MOZ_ENABLE_WAYLAND="1";             # tell firefox to use wayland
+  console = {
+    font = "arm8";
+    keyMap = "us";
   };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
+  # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -66,16 +51,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.dhpham = {
@@ -84,16 +60,13 @@
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       firefox
+      tree
     ];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
   # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     wget
     git
     alacritty
@@ -103,43 +76,22 @@
     enable = true;
     config = {
       init.defaultBranch = "main";
-      user.email = "dayvidpham@gmail.com";
       user.name = "dayvidpham";
+      user.email = "dayvidpham@gmail.com";
     };
   };
 
-  # DEPRECATED
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # services.xserver.displayManager.gdm.enable = false;
-
-  # # Configure keymap in X11
-  # services.xserver = {
-  #   layout = "us";
-  #   xkbVariant = "";
-  # };
-
-  # Enable automatic login for the user.
-  # services.xserver.displayManager.autoLogin.enable = true;
-  # services.xserver.displayManager.autoLogin.user = "dhpham";
-
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # Window manager
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
 }
