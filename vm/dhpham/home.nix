1fd924d497cee4b2bf9e1859db6f083bfe19e9e7
dayvidpham <dayvidpham@gmail.com>
2023-11-27 00:52:07 -0800

    chore: Move config to top of file

--- vm/dhpham/home.nix
+++ vm/dhpham/home.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "dhpham";
@@ -50,8 +53,4 @@
     EDITOR = "vim";
     VISUAL = "vim";
   };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
 }
