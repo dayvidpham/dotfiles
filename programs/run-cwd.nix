0299463d26ad87307e5bf76e71b47f066cf5f937
dayvidpham <dayvidpham@gmail.com>
2024-05-03 00:41:47 -0700

    Tries to fix run-cwd

--- programs/run-cwd.nix
+++ programs/run-cwd.nix
@@ -9,7 +9,9 @@ writeShellApplication rec {
     runtimeInputs = [ sway jq ];
     text = ''
         #!${runtimeShell}
-        if FOCUSED=$(swaymsg -t get_tree | jq -e '.. | select(.type?) | select(.focused) | .pid') && [ -n "$FOCUSED" ]; then
+        FOCUSED=$(swaymsg -t get_tree | jq -e '.. | select(.type?) | select(.focused) | .pid');
+        echo $FOCUSED
+        if [ -n "$FOCUSED" ]; then
             # cwd of first-level child is usually more useful (e.g. shell proc forked from terminal emulator)
             # but fallback to the cwd of the focused app if no children procs
             for pid in $(cat "/proc/$FOCUSED/task"/*/children) $FOCUSED; do
