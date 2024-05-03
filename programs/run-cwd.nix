bf80968e366dedc2c7ffe4c2cb9c1b29a0d44c8a
dayvidpham <dayvidpham@gmail.com>
2024-05-03 00:48:06 -0700

    Adds double quotes around Bash variable

--- programs/run-cwd.nix
+++ programs/run-cwd.nix
@@ -10,7 +10,7 @@ writeShellApplication rec {
     text = ''
         #!${runtimeShell}
         FOCUSED=$(swaymsg -t get_tree | jq -e '.. | select(.type?) | select(.focused) | .pid');
-        echo $FOCUSED
+        echo "$FOCUSED"
         if [ -n "$FOCUSED" ]; then
             # cwd of first-level child is usually more useful (e.g. shell proc forked from terminal emulator)
             # but fallback to the cwd of the focused app if no children procs
