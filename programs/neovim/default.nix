4f53ccbae45128e25c03b61866ece0df5193d300
dayvidpham <dayvidpham@gmail.com>
2024-05-03 23:43:01 -0700

    Adds kickstart nvim with TreeSitter

---
+++ programs/neovim/default.nix
@@ -0,0 +1,66 @@
+{ pkgs, ... }:
+let
+  treesitterWithGrammars = (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
+    p.bash
+    p.comment
+    p.css
+    p.dockerfile
+    p.gitattributes
+    p.gitignore
+    p.javascript
+    p.jq
+    p.json5
+    p.json
+    p.lua
+    p.make
+    p.markdown
+    p.nix
+    p.python
+    p.toml
+    p.typescript
+    p.yaml
+  ]));
+
+  treesitter-parsers = pkgs.symlinkJoin {
+    name = "treesitter-parsers";
+    paths = treesitterWithGrammars.dependencies;
+  };
+in
+{
+  home.packages = with pkgs; [
+    ripgrep
+    fd
+    lua-language-server
+    rust-analyzer-unwrapped
+    black
+  ];
+
+  programs.neovim = {
+    enable = true;
+    package = pkgs.neovim-nightly;
+    vimAlias = true;
+    coc.enable = false;
+    withNodeJs = true;
+
+    plugins = [
+      treesitterWithGrammars
+    ];
+  };
+
+  home.file."./.config/nvim/" = {
+    source = ./nvim;
+    recursive = true;
+  };
+
+  home.file."./.config/nvim/lua/minttea/init.lua".text = ''
+    vim.opt.runtimepath:append("${treesitter-parsers}")
+  '';
+
+  # Treesitter is configured as a locally developed module in lazy.nvim
+  # we hardcode a symlink here so that we can refer to it in our lazy config
+  home.file."./.local/share/nvim/nix/nvim-treesitter/" = {
+    recursive = true;
+    source = treesitterWithGrammars;
+  };
+
+}
