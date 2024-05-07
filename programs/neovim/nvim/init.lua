4a550b83429f0db98bcd2211e097aee5c0a16bae
dayvidpham <dayvidpham@gmail.com>
2024-05-06 17:54:02 -0700

    Render all spaces

--- programs/neovim/nvim/init.lua
+++ programs/neovim/nvim/init.lua
@@ -144,7 +144,7 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = '» ', space = '·', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
