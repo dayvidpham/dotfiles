84fcf31c6d363a24cc6cc447926c364c654bf7d7
David Huu Pham <dayvidpham@gmail.com>
2024-05-11 09:15:39 -0700

    revert: Default boot settings: chooses latest nix

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -26,13 +26,13 @@
   boot = {
     # Use the systemd-boot EFI boot loader.
     loader.systemd-boot.enable = true;
-    loader.systemd-boot.extraFiles = {
-      "loader/loader.conf" = pkgs.writeText "loader.conf" ''
-          timeout 10
-          default @saved
-          console-mode keep
-        '';
-    };
+    #loader.systemd-boot.extraFiles = {
+    #  "loader/loader.conf" = pkgs.writeText "loader.conf" ''
+    #      timeout 10
+    #      default @saved
+    #      console-mode keep
+    #    '';
+    #};
 
     loader.efi.canTouchEfiVariables = true;
     kernelPackages = pkgs.linuxPackages_latest;
