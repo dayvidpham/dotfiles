e1c65118e668d1eeb7a911221b6f36bbcfff41a1
David Huu Pham <dayvidpham@gmail.com>
2024-06-12 20:24:25 -0700

    feat: cleans up the nvidia module, per-host config

--- modules/hardware/nvidia/default.nix
+++ modules/hardware/nvidia/default.nix
@@ -2,10 +2,63 @@
   config
   , pkgs
   , lib ? pkgs.lib
+  , builtins
   , ... 
 }:
 let
   cfg = config.CUSTOM.hardware.nvidia;
+
+  # NOTE: Utils
+  configureHost = let
+    getHostConfig = hostName: options:
+      if   (builtins.hasAttr hostName options)
+      then options.${hostName}
+      else options.default;
+  in hostName: optionsSet:
+    mapAttrs (optionKey: optionValueSet: getHostConfig hostName optionValueSet) 
+    optionsSet;
+
+
+  # NOTE: Config
+  nvidia = {
+    powerManagement = rec {
+      default = {
+        enable      = true;         # Enable dGPU systemd power management
+        finegrained = true;         # Enable PRIME offload power management
+      };
+
+      desktop = default // {
+        enable      = false;
+        finegrained = false;
+      };
+
+      flowX13 = default;
+    };
+
+    # NOTE: Balancing between iGPU and dGPU
+    prime = rec {
+      # NOTE: Enable better balancing between CPU and iGPU
+      #dynamicBoost.enable = true;   
+
+      # NOTE: Sync and Offload mode cannot be used at the same time
+      #offload.enable = true;            # Enable offloading to dGPU
+      #offload.enableOffloadCmd = true;  # convenience script to run on dGPU
+
+      default = config.nvidia.hardware.prime;
+
+      desktop = default // {
+        sync.enable = true;         # Use dGPU for everything
+        nvidiaBusId = "PCI:1:0:0";
+        amdgpuBusId = "PCI:16:0:0";
+      };
+    };
+
+    # NOTE: Open kernel module: this is not the nouveau driver
+    open = {
+      default = false; # GTX 10XX gen is unsupported
+                       # we on the RTX 4090 now though!
+    };
+  };
 
   inherit (lib) 
     mkIf
@@ -17,10 +70,15 @@ in {
     CUSTOM.hardware.nvidia = {
       enable = 
         mkEnableOption "NVIDIA GPU settings for various hosts";
+
       proprietaryDrivers.enable = 
         mkEnableOption "proprietary NVIDIA drivers" // { 
           default = true;
         };
+
+      hostName =
+        mkDefaultOption config.networking.hostName {};
+
       package = 
         mkPackageOption config.boot.kernelPackages.nvidiaPackages "stable" {};
     };
@@ -28,55 +86,35 @@ in {
 
   config = mkIf cfg.enable {
     hardware.opengl = {
-      enable = true;
-      driSupport = true;
+      enable          = true;
+      driSupport      = true;
       driSupport32Bit = true;
-      extraPackages = with pkgs; [
+      extraPackages   = with pkgs; [
         nvidia-vaapi-driver
-        vaapiVdpau
       ];
     };
 
     hardware.nvidia = {
-      package = cfg.package;
-      modesetting.enable = true;    # NOTE: Sway will hang if not set
-      nvidiaSettings = true;
-
-      # NOTE: Ryzen 9 7950X3D has iGPU too
-      #dynamicBoost.enable = true;   # Enable better balancing between CPU and iGPU
-      powerManagement = {
-        enable = false;              # Enable dGPU systemd power management
-          finegrained = false;         # Enable PRIME offload power management
-      };
-      # Balancing between iGPU and dGPU
-      prime = {
-        sync.enable = true;         # Use dGPU for everything
-      
-      # NOTE: Sync and Offload mode cannot be used at the same time
-      #offload.enable = true;            # Enable offloading to dGPU
-      #offload.enableOffloadCmd = true;  # convenience script to run on dGPU
-      
-                nvidiaBusId = "PCI:1:0:0";
-              amdgpuBusId = "PCI:16:0:0";
-            };
-      
-      # NOTE: If screen tearing persists, might want to disable this
-      # Open kernel module: this is not the nouveau driver
-            open = false; # GTX 10XX gen is unsupported
-      # we on the RTX 4090 now though!
-          };
-          services.xserver = {
-            enable = true;
-            videoDrivers = [ "nvidia" "amdgpu" ];    # NOTE: If commented, will use nouveau drivers
-              xkb.variant = "";
-            xkb.layout = "us";
-          };
-      
-      # NOTE: To load nvidia drivers first
-          boot.initrd.kernelModules = [ "nvidia" ];
-          boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
-      # NOTE: Maybe fixes white screen flickering with AMD iGPU
-    boot.kernelParams = [ "amdgpu.sg_display=0" ];
+      package             = cfg.package;
+      modesetting.enable  = true;    # NOTE: Sway will hang if not set
+      nvidiaSettings      = true;
+    } // configureHost cfg.hostName cfg.nvidia;
 
     programs.xwayland.enable = true;
+    services.xserver = {
+      enable = true;
+      videoDrivers = # NOTE: If not set, will use nouveau drivers
+        mkIf cfg.proprietaryDrivers.enable [ "nvidia" ];
+    };
+
+    boot = # NOTE: To load nvidia drivers first
+      mkIf cfg.proprietaryDrivers.enable {
+        initrd.kernelModules = [ "nvidia" ];
+        extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+      };
+
+    # NOTE: Maybe fixes white screen flickering with AMD iGPU
+    # videoDrivers = [ "amdgpu" ];    # NOTE: If commented, will use nouveau drivers
+    # boot.kernelParams = [ "amdgpu.sg_display=0" ];
+  };
 }
