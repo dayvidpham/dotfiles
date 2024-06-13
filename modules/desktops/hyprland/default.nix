e3815d926f44af0e12ca2438b1f499d5439ba771
David Huu Pham <dayvidpham@gmail.com>
2024-06-12 22:21:53 -0700

    fix: duplicated modules

--- modules/desktops/hyprland/default.nix
+++ modules/desktops/hyprland/default.nix
@@ -13,21 +13,21 @@ let
     mkPackageOption
   ;
 in {
-  options = {
-    CUSTOM.v4l2loopback = {
-      enable = mkEnableOption "v4l2loopback";
-      kernelPackage = mkPackageOption config.boot.kernelPackages "v4l2loopback" {};
-      utilsPackage = mkPackageOption pkgs "v4l-utils" {};
-    };
-  };
+  #options = {
+  #  CUSTOM.v4l2loopback = {
+  #    enable = mkEnableOption "v4l2loopback";
+  #    kernelPackage = mkPackageOption config.boot.kernelPackages "v4l2loopback" {};
+  #    utilsPackage = mkPackageOption pkgs "v4l-utils" {};
+  #  };
+  #};
 
-  config = mkIf cfg.enable {
-    boot.kernelModules = [ "v4l2loopback" ];
-    boot.extraModulePackages = [ 
-      cfg.kernelPackage
-    ];
-    environment.systemPackages = [ 
-      cfg.utilsPackage
-    ];
-  };
+  #config = mkIf cfg.enable {
+  #  boot.kernelModules = [ "v4l2loopback" ];
+  #  boot.extraModulePackages = [ 
+  #    cfg.kernelPackage
+  #  ];
+  #  environment.systemPackages = [ 
+  #    cfg.utilsPackage
+  #  ];
+  #};
 }
