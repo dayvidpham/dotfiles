366fd56cf96efc90331dd362b7e07ae2486b7997
David Huu Pham <dayvidpham@gmail.com>
2024-06-20 01:09:29 -0700

    fix: links monitors when scaling applied

--- modules/home-manager/services/kanshi/default.nix
+++ modules/home-manager/services/kanshi/default.nix
@@ -1,21 +1,22 @@
-{
-  config
-  , pkgs
-  , lib ? pkgs.lib
-  , terminal
-  , menu
-  , ...
-}: let
+{ config
+, pkgs
+, lib ? pkgs.lib
+, terminal
+, menu
+, ...
+}:
+let
   cfg = config.CUSTOM.services.kanshi;
 
   inherit (lib)
     mkIf
     mkEnableOption
-  ;
+    ;
 
-in {
+in
+{
   options.CUSTOM.services.kanshi = {
-    enable = 
+    enable =
       mkEnableOption "setup Wayland displays to personal preference";
   };
 
@@ -31,83 +32,83 @@ in {
       settings = [
 
         {
-          profile.name    = "desktop-1";
+          profile.name = "desktop-1";
           profile.outputs = [
 
             # NOTE: Desktop, 3 monitor setup: |V|[ H ]|V|
             {
               # left
-              criteria      = "DP-3";
-              mode          = "2560x1440@169.831Hz";
-              #scale         = 1.5;
-              position      = "0,0";
-              adaptiveSync  = true;
-              transform     = "270";
+              criteria = "DP-3";
+              mode = "2560x1440@169.831Hz";
+              scale = 1.25;
+              position = "0,0";
+              adaptiveSync = true;
+              transform = "270";
             }
             {
               # center
-              criteria      = "DP-2";
-              mode          = "2560x1440@169.831Hz";
-              #scale         = 1.5;
-              position      = "1440,374";
-              adaptiveSync  = true;
+              criteria = "DP-2";
+              mode = "2560x1440@169.831Hz";
+              scale = 1.25;
+              position = "1152,374";
+              adaptiveSync = true;
             }
             {
               # right
-              criteria      = "DP-1";
-              mode          = "2560x1440@169.831Hz";
-              #scale         = 1.5;
-              position      = "4000,0";
-              adaptiveSync  = true;
-              transform     = "270";
+              criteria = "DP-1";
+              mode = "2560x1440@169.831Hz";
+              scale = 1.25;
+              position = "3200,0";
+              adaptiveSync = true;
+              transform = "270";
             }
 
           ];
         }
 
         {
-          profile.name    = "desktop-2";
+          profile.name = "desktop-2";
           profile.outputs = [
 
             # NOTE: Desktop, 3 monitor setup: |V|[ H ]|V|
             {
               # left
-              criteria      = "DP-6";
-              mode          = "2560x1440@169.831Hz";
-              #scale         = 1.5;
-              position      = "0,0";
-              adaptiveSync  = true;
-              transform     = "270";
+              criteria = "DP-6";
+              mode = "2560x1440@169.831Hz";
+              scale = 1.25;
+              position = "0,0";
+              adaptiveSync = true;
+              transform = "270";
             }
             {
               # center
-              criteria      = "DP-5";
-              mode          = "2560x1440@169.831Hz";
-              #scale         = 1.5;
-              position      = "1440,374";
-              adaptiveSync  = true;
+              criteria = "DP-5";
+              mode = "2560x1440@169.831Hz";
+              scale = 1.25;
+              position = "1152,374";
+              adaptiveSync = true;
             }
             {
               # right
-              criteria      = "DP-4";
-              mode          = "2560x1440@169.831Hz";
-              #scale         = 1.5;
-              position      = "4000,0";
-              adaptiveSync  = true;
-              transform     = "270";
+              criteria = "DP-4";
+              mode = "2560x1440@169.831Hz";
+              scale = 1.25;
+              position = "3200,0";
+              adaptiveSync = true;
+              transform = "270";
             }
 
           ];
         }
-        
+
         # NOTE: Laptop
         {
-          profile.name    = "flowX13";
+          profile.name = "flowX13";
           profile.outputs = [
             {
-              criteria  = "eDP-1";
-              mode      = "1920x1200@119.90Hz";
-              scale     = 1.25;
+              criteria = "eDP-1";
+              mode = "1920x1200@119.90Hz";
+              scale = 1.25;
             }
           ];
         }
