9b18c1c2a54d3773680b280b194883af4136ea23
David Huu Pham <dayvidpham@gmail.com>
2024-06-27 01:39:55 -0700

    fix: activate v4l2loopback kernel module with sane defaults

--- modules/nixos/v4l2loopback/default.nix
+++ modules/nixos/v4l2loopback/default.nix
@@ -1,32 +1,35 @@
-{
-  config
-  , pkgs
-  , lib ? pkgs.lib
-  , ... 
+{ config
+, pkgs
+, lib ? pkgs.lib
+, ...
 }:
 let
   cfg = config.CUSTOM.v4l2loopback;
 
-  inherit (lib) 
+  inherit (lib)
     mkIf
     mkEnableOption
     mkPackageOption
-  ;
-in {
+    ;
+in
+{
   options = {
     CUSTOM.v4l2loopback = {
       enable = mkEnableOption "v4l2loopback";
-      kernelPackage = mkPackageOption config.boot.kernelPackages "v4l2loopback" {};
-      utilsPackage = mkPackageOption pkgs "v4l-utils" {};
+      kernelPackage = mkPackageOption config.boot.kernelPackages "v4l2loopback" { };
+      utilsPackage = mkPackageOption pkgs "v4l-utils" { };
     };
   };
 
   config = mkIf cfg.enable {
     boot.kernelModules = [ "v4l2loopback" ];
-    boot.extraModulePackages = [ 
+    boot.extraModulePackages = [
       cfg.kernelPackage
     ];
-    environment.systemPackages = [ 
+    boot.extraModprobeConfig = ''
+      options v4l2loopback exclusive_caps=1 video_nr=0 card_label="v4l2loopback device"
+    '';
+    environment.systemPackages = [
       cfg.utilsPackage
     ];
   };
