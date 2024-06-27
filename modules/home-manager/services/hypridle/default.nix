426c65d7f0d5df690dbe808abba19278097e14bd
David Huu Pham <dayvidpham@gmail.com>
2024-06-26 19:53:32 -0700

    fix: pass /nix/store path to hypridle script

--- modules/home-manager/services/hypridle/default.nix
+++ modules/home-manager/services/hypridle/default.nix
@@ -11,7 +11,10 @@ let
     mkOption
     mkDefault
     mkEnableOption
+    getExe
     ;
+
+  hyprlockExe = getExe pkgs.hyprlock;
 
 in
 {
@@ -24,9 +27,12 @@ in
       enable = true;
       settings = {
         general = {
-          lock_cmd = "pidof hyprlock || hyprlock"; # avoid starting multiple hyprlock instances.
+          lock_cmd = "pidof ${hyprlockExe} || ${hyprlockExe}"; # avoid starting multiple hyprlock instances.
           before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
           after_sleep_cmd = "hyprctl dispatch dpms on"; # to avoid having to press a key twice to turn on the display.
+          ignore_dbus_inhibit = false; # whether to ignore dbus-sent idle-inhibit requests (used by e.g. firefox or steam)
+          ignore_systemd_inhibit = false; # whether to ignore systemd-inhibit --what=idle inhibitors
+
         };
 
         listener = [
