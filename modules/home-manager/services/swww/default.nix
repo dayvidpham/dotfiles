a0b52966a67062dd927b7d9f5cde83b3d05e995b
David Huu Pham <dayvidpham@gmail.com>
2024-06-27 17:05:07 -0700

    feat(swww): restore cached wallpaper on systemd unit restart

--- modules/home-manager/services/swww/default.nix
+++ modules/home-manager/services/swww/default.nix
@@ -13,9 +13,25 @@ let
     mkPackageOption
     mkEnableOption
     getExe'
+    getExe
     optional
     ;
 
+  defaultTargets = [ "graphical-session.target" ];
+  desktopTargets = defaultTargets ++
+    # Set wallpaper only after displays are reconfigured
+    (optional config.services.kanshi.enable "kanshi.service");
+
+  swww-restore-cache = pkgs.writeShellApplication {
+    name = "swww-restore-cache";
+    runtimeInputs = [ cfg.package ];
+    text = ''
+      centerDisplayCache=$(find "${config.xdg.cacheHome}/swww" -type f -regex '.*\(eDP-1\|DP-2\|DP-5\)$' -regextype posix-extended)
+      # Need to run twice: otherwise animation freezes and only displays single pixel
+      ${getExe cfg.package} img --resize fit -t center --transition-fps 60 "$(cat "$centerDisplayCache")"
+      ${getExe cfg.package} img --resize fit -t center --transition-fps 60 "$(cat "$centerDisplayCache")"
+    '';
+  };
 in
 {
   options.CUSTOM.services.swww = {
@@ -24,7 +40,6 @@ in
     '';
     package = mkPackageOption pkgs "swww" {
       default = "swww";
-      #defaultText = "pkgs.swww";
     };
     systemd.enable = (mkEnableOption ''
       systemd unit that runs swww-daemon
@@ -36,29 +51,23 @@ in
       swww
     ];
 
-    systemd.user.services.swww-daemon =
-      let
-        desktopTargets = [ "graphical-session.target" ];
-        afterKanshi = desktopTargets
-          ++ (optional config.services.kanshi.enable "kanshi.service");
-      in
-      {
-        Install = { WantedBy = desktopTargets; };
+    systemd.user.services.swww-daemon = {
+      Install = { WantedBy = desktopTargets; };
 
-        Unit = {
-          ConditionEnvironment = "WAYLAND_DISPLAY";
-          Description = "swww-daemon (swww wallpaper daemon)";
-          After = afterKanshi;
-          Requires = afterKanshi;
-        };
-
-        Service = {
-          ExecStart = "${getExe' cfg.package "swww-daemon"}";
-          ExecStartPost = "${getExe' cfg.package "swww"} restore";
-          Restart = "always";
-          RestartSec = "10";
-        };
+      Unit = {
+        ConditionEnvironment = "WAYLAND_DISPLAY";
+        Description = "swww-daemon (swww wallpaper daemon)";
+        After = desktopTargets;
+        Requires = desktopTargets;
       };
+
+      Service = {
+        ExecStart = "${getExe' cfg.package "swww-daemon"}";
+        ExecStartPost = "${getExe swww-restore-cache}";
+        Restart = "always";
+        RestartSec = "10";
+      };
+    };
   };
 
 }
