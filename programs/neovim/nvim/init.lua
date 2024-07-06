73b9a0e35bcfbe030911bcc9f08c77ea39f1b1e1
David Huu Pham <dayvidpham@gmail.com>
2024-07-05 20:44:00 -0700

    chore(nvim): more vim-like cmp binds

--- programs/neovim/nvim/init.lua
+++ programs/neovim/nvim/init.lua
@@ -761,9 +761,9 @@ require('lazy').setup({
         -- No, but seriously. Please read `:help ins-completion`, it is really good!
         mapping = cmp.mapping.preset.insert {
           -- Select the [n]ext item
-          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-j>'] = cmp.mapping.select_next_item(),
           -- Select the [p]revious item
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-k>'] = cmp.mapping.select_prev_item(),
 
           -- Scroll the documentation window [b]ack / [f]orward
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -772,7 +772,7 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<C-Enter>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
@@ -984,16 +984,6 @@ require('lazy').setup({
 --  return modname .. path
 --end)
 --
-
-do
-  local Cache = require 'lazy.core.cache'
-  local Util = require 'lazy.core.util'
-  local result = Util.get_unloaded_rtp('', false)
-
-  --for k, v in pairs(result) do
-  --  print(k .. ' = ' .. v)
-  --end
-end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
