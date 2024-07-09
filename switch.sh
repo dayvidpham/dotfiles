9cd7800746ca5b4fbf7f413e56534ccf4db8ee15
David Huu Pham <dayvidpham@gmail.com>
2024-07-08 19:39:35 -0700

    feat(script): wrote a convenience script for switching nixos/hm systems

---
+++ switch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [[ "$#" -eq "0" || "$1" -eq "home" ]]; then
+    home-manager switch --show-trace --flake . |& nom
+elif [[ "$1" -eq "nixos" ]]; then
+    sudo nixos-rebuild switch --show-trace --flake . |& nom
+elif [[ "$#" -gt "1" ]]; then
+    echo 'ERROR: Zero or one argument [home|nixos] only, home if no arguments given'
+fi
+
