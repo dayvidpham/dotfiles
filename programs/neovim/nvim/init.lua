4c864a9ec93a3a7ea225ee6b979c5f4b983291cb
David Huu Pham <dayvidpham@gmail.com>
2024-07-17 14:11:31 -0700

    feat(nvim): set expandtabs and smartindent

--- programs/neovim/nvim/init.lua
+++ programs/neovim/nvim/init.lua
@@ -131,6 +131,8 @@ vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
+vim.opt.smartindent = true
+vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 
