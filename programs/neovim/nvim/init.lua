3dc0a2d170b5261c9d3ad0048680a96fa3ae1180
David Huu Pham <dayvidpham@gmail.com>
2024-12-07 13:50:46 -0800

    chore(nvim): opens file to last editted position

--- programs/neovim/nvim/init.lua
+++ programs/neovim/nvim/init.lua
@@ -226,6 +226,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Open file to last position
+vim.api.nvim_create_autocmd('BufReadPost', {
+  pattern = { '*' },
+  callback = function()
+    if vim.fn.line '\'"' > 1 and vim.fn.line '\'"' <= vim.fn.line '$' then
+      vim.api.nvim_exec2('normal! g\'"', {})
+    end
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
