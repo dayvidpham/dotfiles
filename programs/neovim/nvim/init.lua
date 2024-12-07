78c44e304c993e76b3ead06ee32a32eefda18bd8
David Huu Pham <dayvidpham@gmail.com>
2024-12-07 13:50:57 -0800

    chore(nvim): gets rid of useless groups

--- programs/neovim/nvim/init.lua
+++ programs/neovim/nvim/init.lua
@@ -316,19 +316,12 @@ require('lazy').setup({
         {
           mode = { 'n' },
           { '<leader>c', group = '[C]ode' },
-          { '<leader>c_', hidden = true },
           { '<leader>d', group = '[D]ocument' },
-          { '<leader>d_', hidden = true },
           { '<leader>h', group = 'Git [H]unk' },
-          { '<leader>h_', hidden = true },
           { '<leader>r', group = '[R]ename' },
-          { '<leader>r_', hidden = true },
           { '<leader>s', group = '[S]earch' },
-          { '<leader>s_', hidden = true },
           { '<leader>t', group = '[T]oggle' },
-          { '<leader>t_', hidden = true },
           { '<leader>w', group = '[W]orkspace' },
-          { '<leader>w_', hidden = true },
         },
 
         -- visual mode
