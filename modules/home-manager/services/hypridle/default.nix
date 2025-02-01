0e13c9d3907b99c133ba2558bab3434b7a51f9e7
David Huu Pham <dayvidpham@gmail.com>
2025-02-01 01:00:51 -0800

    chore(hypridle): no longer puts computer to sleep

--- modules/home-manager/services/hypridle/default.nix
+++ modules/home-manager/services/hypridle/default.nix
@@ -62,11 +62,6 @@ in
             on-timeout = "hyprctl dispatch dpms off"; # screen off when timeout has passed
             on-resume = "hyprctl dispatch dpms on"; # screen on when activity is detected after timeout has fired.
           }
-
-          {
-            timeout = "1800"; # 30min
-            on-timeout = "systemctl suspend"; # suspend pc
-          }
         ];
       };
     };
