ca2a5b81a5f2212d38e1651d11da9a32e4e86300
David Huu Pham <dayvidpham@gmail.com>
2025-02-10 02:30:35 -0800

    feat(clipse): adds clipboard manager

--- users/minttea/home.nix
+++ users/minttea/home.nix
@@ -60,6 +60,7 @@ rec {
     # Wayland stuff
     wdisplays # gui for display settings
     wl-clipboard-rs # Rust CLI clipboard utility
+    clipse # CLI clipboard manager
     pw-volume # for volume control w/ sway
     grim # screenshot
     slurp # select region on screen
