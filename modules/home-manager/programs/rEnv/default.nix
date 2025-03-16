94934aa3fdd6dd7b23e0818b61a8c974dc182d50
David Huu Pham <dayvidpham@gmail.com>
2025-03-15 17:57:55 -0700

    feat(rstudio): wraps rstudio with chromium --disable-gpu flag

--- modules/home-manager/programs/rEnv/default.nix
+++ modules/home-manager/programs/rEnv/default.nix
@@ -26,9 +26,19 @@ let
     ];
   });
 
+  f-rstudioWrapperBin = (_pkgs: _pkgs.writeShellApplication {
+    name = "rstudio";
+    runtimeInputs = [
+      (f-rstudio-env _pkgs)
+    ];
+    text = ''
+      RSTUDIO_CHROMIUM_ARGUMENTS="--disable-gpu" rstudio
+    '';
+  });
+
   f-renv = (_pkgs: with _pkgs; [
     R
-    (f-rstudio-env _pkgs)
+    (f-rstudioWrapperBin _pkgs)
   ]);
 in
 {
@@ -42,9 +52,9 @@ in
       pkgs.pandoc
     ] ++ (f-renv pkgs-stable);
 
-    xdg.configFile."rstudio/desktop.info".text = ''
-      [General]
-      desktop.renderingEngine=software
-    '';
+    #xdg.configFile."rstudio/desktop.info".text = ''
+    #  [General]
+    #  desktop.renderingEngine=software
+    #'';
   };
 }
