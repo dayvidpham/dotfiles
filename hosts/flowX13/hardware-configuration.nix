1b08e1080d543af92e151e80fe587d91d8aeee5b
dayvidpham <dayvidpham@gmail.com>
2025-04-09 21:33:34 -0700

    fix(hardware-configuration): missing semi-colon, formatter

--- hosts/flowX13/hardware-configuration.nix
+++ hosts/flowX13/hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
@@ -14,12 +15,14 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/fb7624ac-190d-40a8-aed8-2bba14c69a7c";
+    {
+      device = "/dev/disk/by-uuid/fb7624ac-190d-40a8-aed8-2bba14c69a7c";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/1CB9-3587";
+    {
+      device = "/dev/disk/by-uuid/1CB9-3587";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
@@ -27,7 +30,7 @@
   swapDevices = [
     {
       device = "/swapfile";
-      size = 16 * 1024 # 16 GiB
+      size = 16 * 1024; # 16 GiB
     }
   ];
 
