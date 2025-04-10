0bd0ec37165c37d5853b538d38e8dbf7ba49fb28
dayvidpham <dayvidpham@gmail.com>
2025-04-09 21:23:06 -0700

    chore(swap): enables swapfile by default

--- hosts/flowX13/hardware-configuration.nix
+++ hosts/flowX13/hardware-configuration.nix
@@ -21,9 +21,15 @@
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/1CB9-3587";
       fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 16 * 1024 # 16 GiB
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
