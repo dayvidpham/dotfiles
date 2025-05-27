d615a0a200f51c1a551b58cf02f7477e8607c5eb
dayvidpham <dayvidpham@gmail.com>
2025-05-26 22:40:29 -0700

    feat(scripts/ssh-nohup): uses defaults

--- scripts/ssh-nohup.sh
+++ scripts/ssh-nohup.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env sh
 
-SSH_DEST="desktop"
+source ./lib.sh
+
+SSH_IP_DEFAULT="${home_ip}"
+SSH_USER_DEFAULT="minttea"
+SSH_PORT_DEAFULT="${ssh_port}"
+
+SSH_IP="${SSH_IP:SSH_IP_DEFAULT}"
+SSH_USER="${SSH_USER:SSH_USER_DEFAULT}"
+SSH_PORT="${SSH_PORT:SSH_PORT_DEFAULT}"
+
 OUT_FILE="~/ssh-nohup.txt"
 
-ssh "${SSH_DEST}" $'nohup sh -c \' sleep 10; echo "world"; \' > "${OUT_FILE}" 2>&1 &'
+ssh -p "${SSH_PORT}" "${SSH_USER}@${SSH_IP}" $'nohup sh -c \' python3 src/study/run_tiktok_parallel.py \' > "${OUT_FILE}" 2>&1 &'
