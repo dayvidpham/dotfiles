9dccf3a83d6428ec5ad9b66285fcff8b5f1fc2c6
dayvidpham <dayvidpham@gmail.com>
2025-05-26 23:11:01 -0700

    feat(scripts/nohup): uses printf for better POSIX sh compatibility

--- scripts/ssh-nohup.sh
+++ scripts/ssh-nohup.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env sh
 
-source ./lib.sh
+. ./lib.sh
 
 SSH_IP_DEFAULT="${home_ip}"
 SSH_USER_DEFAULT="minttea"
-SSH_PORT_DEAFULT="${ssh_port}"
+SSH_PORT_DEFAULT="${ssh_port}"
 
-SSH_IP="${SSH_IP:SSH_IP_DEFAULT}"
-SSH_USER="${SSH_USER:SSH_USER_DEFAULT}"
-SSH_PORT="${SSH_PORT:SSH_PORT_DEFAULT}"
+SSH_IP="${SSH_IP:-$SSH_IP_DEFAULT}"
+SSH_USER="${SSH_USER:-$SSH_USER_DEFAULT}"
+SSH_PORT="${SSH_PORT:-$SSH_PORT_DEFAULT}"
 
 OUT_FILE="~/ssh-nohup.txt"
 
-ssh -p "${SSH_PORT}" "${SSH_USER}@${SSH_IP}" $'nohup sh -c \' python3 src/study/run_tiktok_parallel.py \' > "${OUT_FILE}" 2>&1 &'
+SH_CMD_DEFAULT="$(printf "nohup sh -c ' python3 src/study/run_tiktok_parallel.py ' > %s 2>&1 &" "${OUT_FILE}")"
+SH_CMD="${SH_CMD:-$SH_CMD_DEFAULT}"
+
+printf "[INFO] Will attempt to run the command:\n\`\`\`\n%s\n\`\`\`\n\n" "${SH_CMD}"
+
+ssh -p "${SSH_PORT}" "${SSH_USER}@${SSH_IP}" "${SH_CMD}"
