5f1e0c395b35f6da4f1efec940715e2da72c7fd4
David Huu Pham <dayvidpham@gmail.com>
2025-06-02 03:51:59 -0700

    fix(podman): uses user's userspace podman socket

--- scripts/sfurs-gitlab-runner.sh
+++ scripts/sfurs-gitlab-runner.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 docker run -d --name sfurs --restart always \
-  -v /var/run/podman/podman.sock:/var/run/podman/podman.sock \
+  -v "$XDG_RUNTIME_DIR/podman/podman.sock:/var/run/podman/podman.sock" \
   -v gitlab-runner-config:/etc/gitlab-runner \
   gitlab/gitlab-runner:latest
