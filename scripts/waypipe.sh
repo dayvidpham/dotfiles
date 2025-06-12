db29062ba2a0d09978fdb53e66f4e02d715882ed
dayvidpham <dayvidpham@gmail.com>
2025-06-11 17:12:52 -0700

    chore(scripts/waypipe): formatting

--- scripts/waypipe.sh
+++ scripts/waypipe.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-source ./lib.sh
+scripts_dir="${0%/*}"
+. "${scripts_dir}/lib.sh"
 
 waypipe --no-gpu \
-    ssh -t -p ${ssh_port} minttea@${home_ip} 'sway --unsupported-gpu'
-
+    ssh -t -p "${ssh_port}" "minttea@${home_ip}" 'sway --unsupported-gpu'
