4ed74959df3413da9577a1a423d1ce98648bd40d
dayvidpham <dayvidpham@gmail.com>
2025-06-28 11:36:30 -0700

    fix: uses tailnet name

--- scripts/waypipe.sh
+++ scripts/waypipe.sh
@@ -4,4 +4,4 @@ scripts_dir="${0%/*}"
 . "${scripts_dir}/lib.sh"
 
 waypipe --no-gpu \
-    ssh -t -p "${ssh_port}" "minttea@${home_ip}" 'sway --unsupported-gpu'
+	ssh -t "minttea@desktop" 'sway --unsupported-gpu'
