0c5a0a89e4a7d8776f14366d5905b2a0be535204
dayvidpham <dayvidpham@gmail.com>
2025-07-07 16:51:42 -0700

    sets governor to schedutil, power/perf was too aggressive

--- hosts/flowX13/hardware-configuration.nix
+++ hosts/flowX13/hardware-configuration.nix
@@ -74,7 +74,7 @@
     PLATFORM_PROFILE_ON_AC = "performance";
 
     # Set the CPU governor for more control
-    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
+    CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
   };
 }
