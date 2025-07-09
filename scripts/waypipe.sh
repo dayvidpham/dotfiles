a5630e1b16a8d533185c613eae0954b4cdd42180
dayvidpham <dayvidpham@gmail.com>
2025-07-08 20:21:36 -0700

    feat: adds --video hw, bpf args to waypipe

--- scripts/waypipe.sh
+++ scripts/waypipe.sh
@@ -3,5 +3,7 @@
 scripts_dir="${0%/*}"
 . "${scripts_dir}/lib.sh"
 
-waypipe --no-gpu \
+waypipe \
+	--no-gpu \
+	--video "av1,hw,hwenc,hwdec,bpf=8e6" \
 	ssh -t "minttea@desktop" 'sway --unsupported-gpu'
