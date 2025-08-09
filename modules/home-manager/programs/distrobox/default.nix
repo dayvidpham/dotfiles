20c652269756d8cf69c6a2a88dbca9a87ed2a511
David Huu Pham <dayvidpham@gmail.com>
2025-08-08 20:16:54 -0700

    refactor(distrobox): uses sfurs/software image from gitlab registry

--- modules/home-manager/programs/distrobox/default.nix
+++ modules/home-manager/programs/distrobox/default.nix
@@ -22,60 +22,9 @@ in
   config = mkIf cfg.enable {
     programs.distrobox.enable = true;
     programs.distrobox.containers = {
-      debian-common = {
-        image = "debian:13";
-        entry = true;
-        additional_packages = "git";
-        init_hooks = [
-          "ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/docker"
-          "ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/docker-compose"
-        ];
-      };
-
       sfurs = {
-        clone = "debian-common";
-        additional_packages = "
-        git
-        build-essential
-        cmake
-        ninja-build
-        pkg-config
-        qt6-base-dev
-        qt6-declarative-dev
-        qt6-remoteobjects-dev
-        libqt6remoteobjects6
-        libqt6remoteobjects6-bin
-        qml6-module-qtquick
-        qml6-module-qtquick-window
-        qml6-module-qtquick-controls
-        qml6-module-qtquick-templates
-        qml6-module-qtquick-layouts
-        qml6-module-qtqml-workerscript
-        qml6-module-qtremoteobjects
-        libqt5opengl5-dev
-        libgl1-mesa-dev
-        libglu1-mesa-dev
-        libprotobuf-dev
-        protobuf-compiler
-        libode-dev
-        libboost-dev
-        mesa-common-dev
-        libxcb1
-        libxcb-icccm4
-        libxcb-image0
-        libxcb-keysyms1
-        libxcb-randr0
-        libxcb-render-util0
-        libxcb-shape0
-        libxcb-xinerama0
-        libxcb-xkb1
-        libxkbcommon-x11-0
-        python3
-        python3-pip
-        python3-pybind11
-        python3-dev
-      ";
-        entry = false;
+        clone = "registry.gitlab.com/sfurs/software:latest-develop";
+        entry = true;
         nvidia = true;
       };
     };
