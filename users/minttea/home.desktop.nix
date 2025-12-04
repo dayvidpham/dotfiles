48af21f9546ba2951da5781df65c691e99568e12
David Huu Pham <dayvidpham@gmail.com>
2025-12-03 17:37:42 -0800

    feat: tries to install rider and properly integrate with unity

--- users/minttea/home.desktop.nix
+++ users/minttea/home.desktop.nix
@@ -5,18 +5,60 @@
 , osConfig
 , ...
 }:
+let
+  extra-path = with pkgs; [
+    dotnet-sdk_8
+    mono
+    msbuild
+    nuget
+    # Add any extra binaries you want accessible to Rider here
+  ];
+
+
+  extra-lib = with pkgs;[
+    # Add any extra libraries you want accessible to Rider here
+  ];
+
+
+  rider = pkgs.jetbrains.rider.overrideAttrs (attrs: {
+    postInstall = ''
+      # Wrap rider with extra tools and libraries
+
+      mv $out/bin/rider $out/bin/.rider-toolless
+      makeWrapper $out/bin/.rider-toolless $out/bin/rider \
+        --argv0 rider \
+        --prefix PATH : "${lib.makeBinPath extra-path}" \
+        --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath extra-lib}"
+
+      # Making Unity Rider plugin work!
+      # The plugin expects the binary to be at /rider/bin/rider,
+      # with bundled files at /rider/
+      # It does this by going up two directories from the binary path
+      # Our rider binary is at $out/bin/rider, so we need to link $out/rider/ to $out/
+
+      shopt -s extglob
+      ln -s $out/rider/!(bin) $out/
+      shopt -u extglob
+    '' + attrs.postInstall or "";
+  });
+
+in
 {
   home.packages = (with pkgs; [
     # Gaming
     protonup-ng
+
+    # Game Dev
     #godot
     unityhub
-
   ])
   ++ (with pkgs-unstable; [
     # CAD viewer
     #blender
   ])
+  ++ [
+    rider
+  ]
   ;
 
   CUSTOM.games.minecraft.enable = false;
