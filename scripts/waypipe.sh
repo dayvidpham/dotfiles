d9506207775e70fc4fc5ec0b86ab322745555beb
David Huu Pham <dayvidpham@gmail.com>
2026-01-12 11:51:16 -0800

    fix(scripts/waypipe): updates with working config

--- scripts/waypipe.sh
+++ scripts/waypipe.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env sh
 
-scripts_dir="${0%/*}"
-. "${scripts_dir}/lib.sh"
-
-#--no-gpu \
-#--video "av1,hw,hwenc,hwdec,bpf=8e6" \
-waypipe \
-	--remote-node="/etc/card-dgpu" \
-	--video="av1,hw,bpf=8e6" \
-	--debug \
-	ssh -t "minttea@desktop" 'sway --unsupported-gpu'
+waypipe --no-gpu \
+    ssh -t -p 8108 minttea@desktop 'sway --unsupported-gpu'
